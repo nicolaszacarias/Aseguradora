c6d3015ff2f8cc075413ecbc9a9384d43143c5a7
nicolaszacarias <Zacariasnicolas95@gmail.com>
2024-08-15 19:54:26 -0300

    Aseguradora 0,8

--- src/components/companies/Companies.jsx
+++ src/components/companies/Companies.jsx
@@ -7,7 +7,7 @@ const Companies = () => {
         <img src="./triunfo.png" alt="" />
         <img src="./Zurich.png" alt="" />
         <img src="./sancoseg.png" alt="" />
-        <img src="./Rus.png" alt="" />
+        <img src="./norte.png" alt="" />
       </div>
     </section>
   );
